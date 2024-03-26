0690c6876ba20a344936351faf55e29af238a357
Lance Tse <a.tse667@gmail.com>
2024-03-26 13:22:20 -0700

    delete obsolete test routes [id].js and index.js

--- app/pages/api/email/index.js
+++
@@ -1,8 +0,0 @@
-import { sql } from '@vercel/postgres'
-import { NextResponse } from 'next/server'
-
-export default async function handler(req, res) {
-
-    const data = await sql`SELECT email FROM users}`
-    NextResponse.json({ data }, { status: 200 })
-}
