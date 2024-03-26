1f79e14e1da8145818c413d39e0c2a4776deeebf
brandonphillip <drummaster2117@gmail.com>
2024-03-26 12:48:08 -0400

    fixed

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@
 'use client'
 import { StudentProvider } from "./Context/Context";
 import HeaderTemplate from "./Components/HeaderTemplate";
-import LandingPage from './components/LandingPage.jsx'
-import InstructorDashboard from "./components/InstructorView/InstructorDashboard";
+import LandingPage from './Components/LandingPage.jsx'
+import InstructorDashboard from "./Components/InstructorDashboard.jsx";
 
 export default function Home() {
 	return (
