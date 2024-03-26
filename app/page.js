3e8efd1a6b59b822998e42da21c1706d7a657e0a
ObiFlo1 <rubflores1000@gmail.com>
2024-03-26 17:48:45 -0500

    saved most up to date

--- app/page.js
+++ app/page.js
@@ -1,17 +1,25 @@
+// "use client";
+// import { AppProvider } from "./Context/Context";
+// import HeaderTemplate from "./Components/HeaderTemplate";
+
+// export default function Home() {
+//   return (
+//     <>
+//       <AppProvider>
+//         <HeaderTemplate />
+//       </AppProvider>
+//     </>
+//   );
+// }
+
 "use client";
 
-import LandingPage from "./components/LandingPage.jsx";
+import LandingPage from "./Components/LandingPage";
 
 export default function Home() {
   return (
     <>
-      {/* <AppProvider>
-				<HeaderTemplate />
-				<InstructorDashboard />
-			</AppProvider> 
-			<LandingPage />*/}
-
-      <Messages />
+      <LandingPage />
     </>
   );
 
