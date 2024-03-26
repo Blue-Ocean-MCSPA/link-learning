211d181107c5ceec8f521513da8af3a4808a1625
park <minpark93@gmail.com>
2024-03-25 22:17:22 -0700

    working on email/password

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -3,29 +3,49 @@ import React, { useState } from "react";
 const Login = () => {
 
     const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
     }
 
     const handleLoginClick = async (event) => {
         try {
             const response = await fetch(`/api/users`) // all the users
             const data = await response.json();
-            console.log(data.data.rows);
-
-            data.data.rows.map((row) => {
-                if (row.email === email && row.password === password){
-                    console.log("Email found");
-                    console.log(row);
-
-                }
-            })
+            // console.log(data.data.rows);
+            const matchedRows = data.data.rows.filter((row) => {
+                return row.email === email && row.password_hash === password;
+            });
+            if (matchedRows.length > 0) {
+                console.log("Email and password matched");
+                console.log(matchedRows);
+            } else {
+                alert("STOP! You violated the law. Pay the court a fine or serve your sentence. Your stolen goods are now forfeit.");
+            }
+            // let accountFound = false;
+            // data.data.rows.map((row) => {
+            //     if (row.email === email && row.password_hashed === password){
+            //         console.log("Email found");
+            //         console.log(row);
+            //         accountFound = true;
+            //     } else {
+            //         console.log("STOP! You violated the law. Pay the court a fine or serve your sentence. Your stolen goods are now forfeit.")
+            //     }
+            // })
+            // if (!accountFound){
+            //     alert("STOP! You violated the law. Pay the court a fine or serve your sentence. Your stolen goods are now forfeit.")
+            // }
+            }
+            catch(err){
+                console.error(err)
+            }
         }
-        catch(err){
-            console.error(err)
-        }
-    }
 
     return (
         <div className="flex">
@@ -49,7 +69,13 @@ const Login = () => {
                         </div>
                         <div className="pb-2">
                             <label htmlFor="password" className="block text-base mb-2"></label>
-                            <input type="text" id="password" className="bg-light-background border border-2 border-light-comment rounded-full w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-light-inactive_selection" placeholder="Password"/>
+                            <input 
+                            type="text" 
+                            id="password" 
+                            className="bg-light-background border border-2 border-light-comment rounded-full w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-light-inactive_selection" 
+                            placeholder="Password"
+                            onChange={handlePasswordChange}
+                            />
                         </div>
                         <div className="flex justify-center">
                             <div className="w-1/2 text-center mr-4 mt-3 py-1 text-base text-basic-white bg-light-foreground hover:opacity-75 hover:cursor-pointer rounded-full"
