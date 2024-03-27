8684d9e5b450bd7544cd0eb7815cb3bafa53ff9f
park <minpark93@gmail.com>
2024-03-26 20:26:16 -0700

    working on submission

--- app/pages/api/email/[id].js
+++
@@ -1,9 +0,0 @@
-import { sql } from '@vercel/postgres'
-import { NextResponse } from 'next/server'
-
-export default async function handler(req, res) {
-    const { id } = req.query
-    // const data = await sql`SELECT email FROM users WHERE id = ${id}`
-    // NextResponse.json({ data }, { status: 200 })
-    res.status(200).json({ id })
-}
