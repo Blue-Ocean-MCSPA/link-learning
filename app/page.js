6926531ed7a6473200cbde2ee991062ff2077be6
Matt Harlow <mharlow85@gmail.com>
2024-03-26 18:04:10 -0600

    Remove instructor componenent from page, nested in Header

--- app/page.js
+++ app/page.js
@@ -3,14 +3,12 @@
 import { UserProvider } from "./Context/Context";
 import HeaderTemplate from "./Components/HeaderTemplate";
 import LandingPage from './Components/LandingPage.jsx'
-import Instructor from "./Components/InstructorView/Instructor";
 
 export default function Home() {
 	return (
     	<>
 			{/* <UserProvider>
 				<HeaderTemplate />
-				<Instructor />
 			</UserProvider> */}
 			<LandingPage />
     	</>
