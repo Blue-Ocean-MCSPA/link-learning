af5c2c5bb2d072cf445ba2e6762c6a7168145e85
ObiFlo1 <rubflores1000@gmail.com>
2024-03-29 11:34:17 -0500

    ChangedLoggedIn works now

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,106 +1,127 @@
-'use client'
+"use client";
 
 import React, { useState, useContext } from "react";
-import { useAppContext } from "../context";
+import { AppContext } from "../context/index";
 import { useRouter } from "next/navigation";
 
 const Login = () => {
+  const { loggedInRole, setLoggedInRole } = useContext(AppContext);
 
-    const { loggedInRole, changeLoggedInRole } = useAppContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
 
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const router = useRouter();
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    }
+  const changeLoggedInRole = (roleid) => {
+    console.log("set role id to: ", roleid);
+    setLoggedInRole(roleid);
+    console.log("New loggedInRole: ", loggedInRole);
+  };
 
-    const handleLoginClick = async (event) => {
-        try {
-            
-            const response = await fetch(`/api/users`) // all the users
-            const data = await response.json();
-            const matchedRows = data.data.rows.filter((row) => {
-                return row.email === email && row.password_hash === password;
-            });
-                if (matchedRows.length > 0) {
-                    console.log("Email and password matched");
-                    console.log("role id for this matched user: ", matchedRows[0].roleid);
-                    changeLoggedInRole(matchedRows[0].roleid);
-                    console.log("expected: ", matchedRows[0].roleid, "actual: ", loggedInRole)
-                    if (matchedRows[0].roleid === '1') {
-                        router.push('/admin');
-                    } else if (matchedRows[0].roleid === '2') {
-                        console.log("Instructor route pushed");
-                        router.push('/instructor');
-                    } else {
-                        router.push('/student');
-                    }
-                    
-                } else {
-                    alert("STOP! You violated the law. Pay the court a fine or serve your sentence. Your stolen goods are now forfeit.");
-                }
-            }
-            catch(err){
-                console.error(err)
-            }
+  const handleLoginClick = async (event) => {
+    try {
+      const response = await fetch(`/api/users`); // all the users
+      const data = await response.json();
+      const matchedRows = data.data.rows.filter((row) => {
+        return row.email === email && row.password_hash === password;
+      });
+      if (matchedRows.length > 0) {
+        console.log("Email and password matched");
+        console.log("role id for this matched user: ", matchedRows[0].roleid);
+        changeLoggedInRole(matchedRows[0].roleid);
+        console.log(
+          "expected: ",
+          matchedRows[0].roleid,
+          "actual: ",
+          loggedInRole
+        );
+        if (matchedRows[0].roleid === "1") {
+          router.push("/admin");
+        } else if (matchedRows[0].roleid === "2") {
+          console.log("Instructor route pushed");
+          router.push("/instructor");
+        } else {
+          router.push("/student");
         }
+      } else {
+        alert(
+          "STOP! You violated the law. Pay the court a fine or serve your sentence. Your stolen goods are now forfeit."
+        );
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    return (
-        <div className="flex">
-            <div className="w-2/5 flex flex-col justify-center items-center h-screen bg-light-background">
-                <div className="flex flex-col text-6xl w-full justify-center h-1/3">
-                    <div className="text-center tracking-wide font-sans text-light-foreground">
-                        Learning Link
-                    </div>
-                </div>
-                <div className="flex flex-col h-full justify-center items-center w-full bg-light-cursor">
-                    <div className="w-2/3 h-1/2">
-                        <div className="ml-2 mb-5 text-2xl text-light-active_selection">Login to your account</div>
-                        <div className="pb-2">
-                            <label htmlFor="email" className="block text-base mb-2"></label>
-                            <input
-                                type="email"
-                                id="email"
-                                value={email}
-                                placeholder="Email"
-                                autoComplete="email"
-                                className="bg-light-background border border-light-comment rounded-full w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-light-inactive_selection"
-                                onChange={handleEmailChange}
-                            />
-                        </div>
-                        <div className="pb-2">
-                            <label htmlFor="password" className="block text-base mb-2"></label>
-                            <input 
-                                type="text"
-                                id="password"
-                                value={password}
-                                placeholder="Password"
-                                className="bg-light-background border border-light-comment rounded-full w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-light-inactive_selection"
-                                onChange={handlePasswordChange}
-                            />
-                        </div>
-                        <div className="flex justify-center">
-                            <div className="w-1/2 text-center mr-4 mt-3 py-1 text-base text-light-background bg-light-foreground hover:opacity-75 hover:cursor-pointer rounded-full"
-                            onClick={handleLoginClick}
-                            >Login
-                            </div>
-                            <div className="border border-light-background w-1/2 text-center mt-3 py-1 text-base text-light-background hover:text-light-inactive_selection hover:border-light-inactive_selection hover:cursor-pointer rounded-full">Sign Up</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="bg-orange-50 w-3/5 h-full opacity-75">
-                <img src="/fellowMugExtra2.webp" alt="placeholder" className="object-cover"></img>
-            </div>
+  return (
+    <div className="flex">
+      <div className="w-2/5 flex flex-col justify-center items-center h-screen bg-light-background">
+        <div className="flex flex-col text-6xl w-full justify-center h-1/3">
+          <div className="text-center tracking-wide font-sans text-light-foreground">
+            Learning Link
+          </div>
         </div>
-        
-    )
-}
+        <div className="flex flex-col h-full justify-center items-center w-full bg-light-cursor">
+          <div className="w-2/3 h-1/2">
+            <div className="ml-2 mb-5 text-2xl text-light-active_selection">
+              Login to your account
+            </div>
+            <div className="pb-2">
+              <label htmlFor="email" className="block text-base mb-2"></label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                placeholder="Email"
+                autoComplete="email"
+                className="bg-light-background border border-light-comment rounded-full w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-light-inactive_selection"
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div className="pb-2">
+              <label
+                htmlFor="password"
+                className="block text-base mb-2"
+              ></label>
+              <input
+                type="text"
+                id="password"
+                value={password}
+                placeholder="Password"
+                className="bg-light-background border border-light-comment rounded-full w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-light-inactive_selection"
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <div className="flex justify-center">
+              <div
+                className="w-1/2 text-center mr-4 mt-3 py-1 text-base text-light-background bg-light-foreground hover:opacity-75 hover:cursor-pointer rounded-full"
+                onClick={handleLoginClick}
+              >
+                Login
+              </div>
+              <div className="border border-light-background w-1/2 text-center mt-3 py-1 text-base text-light-background hover:text-light-inactive_selection hover:border-light-inactive_selection hover:cursor-pointer rounded-full">
+                Sign Up
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-orange-50 w-3/5 h-full opacity-75">
+        <img
+          src="/fellowMugExtra2.webp"
+          alt="placeholder"
+          className="object-cover"
+        ></img>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- app/context/index.jsx
+++ app/context/index.jsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from "react";
 
 // Import AppContext into your component along with useContext to access state
 export const AppContext = createContext();
@@ -8,129 +8,117 @@ export const AppContext = createContext();
 /****************Context functions****************/
 // Function that stores fetch data from users API hit into state.
 export function AppWrapper({ children }) {
-    // const [users, setUsers] = useState([]);
-    const [loggedInRole, setLoggedInRole] = useState('0'); // [user, setUser
-    const [cohorts, setCohorts] = useState('cohorts');
-    // const [cohortId, setCohortId] = useState(null);
-    // const [students, setStudents] = useState([]);
-    // const [selectedStudent, setSelectedStudent] = useState(null);
-    // const [selectedCohort, setSelectedCohort] = useState(null);
-    // const [selectedTab, setSelectedTab] = useState('students');
-    // const [selectedRole, setSelectedRole] = useState("Admin");
-    const [ darkMode, setDarkMode ] = useState(false);
+  // const [users, setUsers] = useState([]);
+  const [loggedInRole, setLoggedInRole] = useState("0"); // [user, setUser
+  const [cohorts, setCohorts] = useState("cohorts");
+  // const [cohortId, setCohortId] = useState(null);
+  // const [students, setStudents] = useState([]);
+  // const [selectedStudent, setSelectedStudent] = useState(null);
+  // const [selectedCohort, setSelectedCohort] = useState(null);
+  // const [selectedTab, setSelectedTab] = useState('students');
+  // const [selectedRole, setSelectedRole] = useState("Admin");
+  const [darkMode, setDarkMode] = useState(false);
 
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-      };
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
-    const changeLoggedInRole = (roleid) => {
-        console.log("set role id to: ", roleid);
-        setLoggedInRole(roleid);
-        console.log("New loggedInRole: ", loggedInRole)
+  // Function to fetch users from the database and add them to state
+  const fetchInstructorCohorts = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/cohort/${id}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch cohorts");
+      }
+      const data = await response.json();
+      setCohorts(data.instructorCohorts.rows);
+    } catch (error) {
+      console.error("Error fetching cohorts:", error);
+      setCohorts([]);
     }
+  };
 
-    // Function to fetch users from the database and add them to state
-    const fetchInstructorCohorts = async (id) => {
-        try {
-            const response = await fetch(`http://localhost:3000/api/cohort/${id}`);
-            if (!response.ok) {
-                throw new Error('Failed to fetch cohorts');
-            }
-            const data = await response.json();
-            setCohorts(data.instructorCohorts.rows);
-        } catch (error) {
-            console.error('Error fetching cohorts:', error);
-            setCohorts([]);
-        }
+  const fetchCohorts = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/cohort/");
+      if (!response.ok) {
+        throw new Error("Failed to fetch cohorts");
+      }
+      const data = await response.json();
+      setCohorts(data.cohorts.rows);
+      console.log("Cohorts fetched:", data.cohorts.rows);
+    } catch (error) {
+      console.error("Error fetching cohorts:", error);
+      setCohorts([]);
     }
+  };
 
-    const fetchCohorts = async () => {
-        try {
-            const response = await fetch('http://localhost:3000/api/cohort/');
-            if (!response.ok) {
-                throw new Error('Failed to fetch cohorts');
-            }
-            const data = await response.json();
-            setCohorts(data.cohorts.rows);
-            console.log('Cohorts fetched:', data.cohorts.rows);
-        } catch (error) {
-            console.error('Error fetching cohorts:', error);
-            setCohorts([]);
-        }
+  const fetchStudentsInCohort = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/students/${id}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch students belonging to cohort");
+      }
+      const data = await response.json();
+      console.log(data);
+      setStudents(data.studentsInCohort.rows);
+      console.log("Students fetched:", students);
+    } catch (error) {
+      console.error("Error fetching students:", error);
+      setStudents([]);
     }
-    
-    const fetchStudentsInCohort = async (id) => {
-        try {
-            const response = await fetch(`http://localhost:3000/api/students/${id}`);
-            if (!response.ok) {
-                throw new Error('Failed to fetch students belonging to cohort');
-            }
-            const data = await response.json();
-            console.log(data)
-            setStudents(data.studentsInCohort.rows);
-            console.log('Students fetched:', students);
-        } catch (error) {
-            console.error('Error fetching students:', error);
-            setStudents([]);
-        }
-    }
-    
-    const fetchUsers = async () => {
-        try {
-            const response = await fetch('http://localhost:3000/api/users');
-            const data = await response.json();
-            setUsers([{
-                user: data,
-                loading: false
-            }]);
-            console.log('Users fetched:', data);
-        } catch (error) {
-            console.error('Error fetching users:', error);
-            setUsers({
-                users: [],
-                loading: false
-            });
-        }
-    };
-    // Return student data and fetch function.
-    return (
-        <AppContext.Provider value={{
-            // users,
-            // setUsers,
-            loggedInRole,
-            changeLoggedInRole,
-            cohorts,
-            setCohorts,
-            // selectedCohort,
-            // setSelectedCohort,
-            // cohortId,
-            // setCohortId,
-            // fetchCohorts,
-            // fetchInstructorCohorts,
-            // students,
-            // setStudents,
-            // selectedStudent,
-            // setSelectedStudent,
-            // fetchStudentsInCohort,
-            // fetchUsers,
-            // selectedTab,
-            // setSelectedTab,
-            // selectedRole,
-            // setSelectedRole,
-            darkMode,
-            toggleDarkMode
-        }}>
-            {children}
-        </AppContext.Provider>
-    );
-}
+  };
 
-export function useAppContext(){
-    const context = useContext(AppContext);
-    if(!context){
-        throw new Error (
-            "useAppContect must be used within an AppContextProvider"
-        )
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
+      setUsers([
+        {
+          user: data,
+          loading: false,
+        },
+      ]);
+      console.log("Users fetched:", data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      setUsers({
+        users: [],
+        loading: false,
+      });
     }
-    return context;
+  };
+  // Return student data and fetch function.
+  return (
+    <AppContext.Provider
+      value={{
+        // users,
+        // setUsers,
+        loggedInRole,
+        setLoggedInRole,
+        cohorts,
+        setCohorts,
+        // selectedCohort,
+        // setSelectedCohort,
+        // cohortId,
+        // setCohortId,
+        // fetchCohorts,
+        // fetchInstructorCohorts,
+        // students,
+        // setStudents,
+        // selectedStudent,
+        // setSelectedStudent,
+        // fetchStudentsInCohort,
+        // fetchUsers,
+        // selectedTab,
+        // setSelectedTab,
+        // selectedRole,
+        // setSelectedRole,
+        darkMode,
+        toggleDarkMode,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
