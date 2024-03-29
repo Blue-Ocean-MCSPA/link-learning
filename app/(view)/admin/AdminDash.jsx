89f7966db7a357e3cc6d058b98c606a29b885ec5
ObiFlo1 <rubflores1000@gmail.com>
2024-03-29 11:21:59 -0500

    Butter

--- app/(view)/admin/AdminDash.jsx
+++ app/(view)/admin/AdminDash.jsx
@@ -8,26 +8,26 @@ import SelectIntructors from "./SelectInstuctors";
 
 const AdminDashboard = ({ setSelectedCohort, setSelectedInstructor }) => {
   //const { cohorts, setCohorts } = useContext(AppContext);
-  const [ cohorts, setCohorts ] = useState([]);
+  const [cohorts, setCohorts] = useState([]);
 
   const fetchCohorts = async () => {
     try {
-        const response = await fetch('http://localhost:3000/api/cohort/');
-        if (!response.ok) {
-            throw new Error('Failed to fetch cohorts');
-        }
-        const data = await response.json();
-        setCohorts(data.cohorts.rows);
-        console.log('Cohorts fetched:', data.cohorts.rows);
+      const response = await fetch("http://localhost:3000/api/cohort/");
+      if (!response.ok) {
+        throw new Error("Failed to fetch cohorts");
+      }
+      const data = await response.json();
+      setCohorts(data.cohorts.rows);
+      console.log("Cohorts fetched:", data.cohorts.rows);
     } catch (error) {
-        console.error('Error fetching cohorts:', error);
-        setCohorts([]);
+      console.error("Error fetching cohorts:", error);
+      setCohorts([]);
     }
-}
+  };
 
-useEffect(() => {
-  fetchCohorts();
-}, [])
+  useEffect(() => {
+    fetchCohorts();
+  }, []);
 
   const cohortClick = (cohort) => {
     setSelectedCohort(cohort);
@@ -38,8 +38,8 @@ useEffect(() => {
   };
   return (
     <>
-      <div className="flex flex-wrap justify-center items-center h-screen bg-slate-600 border-4 border-purple-500">
-        <div className="flex justify-center  p-5 m-4">
+      <div className="flex-col flex-wrap justify-center items-center h-screen bg-slate-600 ">
+        <div className="flex justify-evenly">
           {/* <button className="bg-slate-800 p-6 m-4 border rounded">
               view all Cohorts
             </button> */}
@@ -54,17 +54,19 @@ useEffect(() => {
             view all Students
           </button>
         </div>
-        {cohorts.map((cohort, index) => {
-          return (
-            <div
-              key={index}
-              className="bg-slate-800 p-6 m-4 border rounded"
-              onClick={() => cohortClick(cohort)}
-            >
-              <h2>{cohort.cohort_name}</h2>
-            </div>
-          );
-        })}
+        <div className="flex justify-center p-6">
+          {cohorts.map((cohort, index) => {
+            return (
+              <div
+                key={index}
+                className="flex items-align bg-slate-800 p-5 m-4 rounded"
+                onClick={() => cohortClick(cohort)}
+              >
+                <h2>{cohort.cohort_name}</h2>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
