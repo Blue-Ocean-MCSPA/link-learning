8256f7e8e899419894c93dce634fec15eeb38f40
ObiFlo1 <rubflores1000@gmail.com>
2024-03-30 02:53:39 -0500

    deleted admin conditional render componentreplaced it with with just admin dash since the login page routes straight to admin dash anyway

--- app/(Components)/admin/Admin.jsx
+++ app/(Components)/admin/Admin.jsx
@@ -1,77 +1,54 @@
-import React, { useState, useContext, useEffect } from "react";
-import AppContext, { useAppContext } from "@/app/context";
-import AdminDash from "./AdminDash";
-import CohortOverview from "./CohortOverview";
-import SelectInstructors from "./SelectInstuctors";
+// import React, { useState, useContext, useEffect } from "react";
+// import AppContext, { AppWrapper, useAppContext } from "@/app/context";
+// import AdminDash from "./AdminDash";
+// import CohortOverview from "./CohortOverview";
+// import SelectInstructors from "./SelectInstuctors";
 
-//This page is green. This is the page that shows all the buttons to click on view all
-//instructors, students, cohorts
+// //This page is green. This is the page that shows all the buttons to click on view all
+// //instructors, students, cohorts
 
-//This is the main parent it looks like
-//I have a red big border in this one
+// //This is the main parent it looks like
+// //I have a red big border in this one
 
-const Admin = () => {
-  const { loggedInRole } = useAppContext(); 
-  const [selectedCohort, setSelectedCohort] = useState(null);
-  const [selectedInstructor, setSelectedInstructor] = useState(null);
+// const Admin = () => {
+//   const { loggedInRole } = useAppContext();
+//   const [selectedCohort, setSelectedCohort] = useState(null);
+//   const [selectedInstructor, setSelectedInstructor] = useState(null);
 
-  return (
-    <div className="">
-      {(() => {
-        switch (true) {
-          case !!selectedCohort:
-            return (
-              <CohortOverview
-                selectedCohort={selectedCohort}
-                setSelectedCohort={setSelectedCohort}
-              />
-            );
-          case !!selectedInstructor:
-            return (
-              <SelectInstructors
-                setSelectedInstructor={setSelectedInstructor}
-              />
-            );
-          default:
-            return (
-                <AdminDash
-                  setSelectedCohort={setSelectedCohort}
-                  setSelectedInstructor={setSelectedInstructor}
-                />
-            );
-        }
-      })()}
-    </div>
-  );
-};
+//   return (
+//     <div className="">
+//       {(() => {
+//         switch (true) {
+//           case !!selectedCohort:
+//             return (
+//               <CohortOverview
+//                 selectedCohort={selectedCohort}
+//                 setSelectedCohort={setSelectedCohort}
+//               />
+//             );
+//           case !!selectedInstructor:
+//             return (
+//               <SelectInstructors
+//                 setSelectedInstructor={setSelectedInstructor}
+//               />
+//             );
+//           default:
+//             return (
+//               <AppWrapper>
+//                 <AdminDash
+//                   setSelectedCohort={setSelectedCohort}
+//                   setSelectedInstructor={setSelectedInstructor}
+//                 />
+//               </AppWrapper>
+//             );
+//         }
+//       })()}
+//     </div>
+//   );
+// };
 
-export default Admin;
+// export default Admin;
 
-// switch case instead of turnary
-// use a pice of state specifically for rendering in that switch case
-//more useful if piece of state is string
-
-{
-  /* <div>
-{selectedCohort ? (
-  <CohortOverview
-    selectedCohort={selectedCohort}
-    setSelectedCohort={setSelectedCohort}
-  />
-) : (
-  <AdminDash
-    setSelectedCohort={setSelectedCohort}
-    setSelectedInstructor={setSelectedInstructor}
-  />
-)}
-
-{selectedInstructor ? (
-  <SelectIntructors />
-) : (
-  <AdminDash
-    setSelectedCohort={setSelectedCohort}
-    setSelectedInstructor={setSelectedInstructor}
-  />
-)}
-</div> */
-}
+// // switch case instead of turnary
+// // use a pice of state specifically for rendering in that switch case
+// //more useful if piece of state is string
--- app/(Components)/admin/page.jsx
+++ app/(Components)/admin/page.jsx
@@ -9,7 +9,7 @@ export default function AdminPage() {
     <>
       <AppWrapper>
         <HeaderTemplate />
-        <Admin />
+        <AdminDash />
       </AppWrapper>
     </>
   );
