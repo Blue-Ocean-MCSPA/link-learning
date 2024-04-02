396c657cd057fe8aabe2cd71677fba7c233227fe
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2024-04-02 17:05:28 -0400

    Route fic

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -2,11 +2,8 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
 export async function GET(request) {
-
     const data = await sql`SELECT * FROM users`;
     return NextResponse.json({ data }, { status: 200 });
-
-
 }
 
 export async function POST(request) {
