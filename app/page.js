b54fbc4645bdfd3a3c3019309b0591724e5d70da
Humberto Palacios <palacioshumberto35@yahoo.com>
2024-04-04 09:49:40 -0700

    new stuff

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 "use client";
 import LandingPage from "./Components/LandingPage.jsx";
+import Messages from "./Components/msg/messages.jsx";
 import { AppWrapper } from "./context/index.jsx";
 
 export default function Home() {
   return (
-      <LandingPage />
+      <Messages />
   );
 }
